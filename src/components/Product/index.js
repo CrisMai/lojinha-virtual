39e8619ccc99b9ba9962d3fcd339987f16c201e2
Cris Mai <crismai.dev@gmail.com>
2022-02-08 15:47:55 -0300

    Ajuste layout table produtos

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -4,15 +4,19 @@ import './styles.css';
 
 export default function Product({ car, addItemCart }) {
 	return (
-		<div className="col-sm-3 mt-3 mb-3 card-total"> 
-			<div className="card"> 
-				<img src={car.image} className="card-img-top" alt={car.title} />
-				<div className="card-body">
-					<h5 className="card-title">{car.title}</h5>
-					<button className="btn btn-primary" onClick={() => addItemCart(car)}>
-						<i className="fa fa-cart-plus fa-2x" aria-hidden="true" />
-					</button>
+		<div className="col-sm-3 mt-3 mb-3">
+			<div className="card">
+				<div className='card-header'>
+					<span data-tooltip={car.title}><h5 className="card-title">{car.title}</h5></span>
 				</div>
+				<div className="card-body">
+					<div className="ratio ratio-16x9">
+						<img src={car.image} className="card-img-top" alt={car.title} />
+					</div>
+				</div>
+				<button className="btn btn-primary" onClick={() => addItemCart(car)}>
+					<i className="fa fa-cart-plus fa-2x" aria-hidden="true" />
+				</button>
 			</div>
 		</div>
 	);
